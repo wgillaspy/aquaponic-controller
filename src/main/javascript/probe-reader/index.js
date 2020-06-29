fce31e32748e71562aaf91f792e3badf0c15a884
William Gillaspy <w.gillaspy@gmail.com>
2020-06-28 20:25:12 -0400

    changes

--- src/main/javascript/probe-reader/index.js
+++ src/main/javascript/probe-reader/index.js
@@ -1,2 +1,11 @@
 const i2c = require('i2c-bus');
 
+const phAddress = 0x99;
+const conductivityAddress = 0x100;
+
+const ICT_SLAVE = 0x703;
+
+const i2c1 = i2c.openSync(1);
+const rawData = i2c1.readWordSync(MCP9808_ADDR, TEMP_REG);
+console.log(rawData);
+i2c1.closeSync();

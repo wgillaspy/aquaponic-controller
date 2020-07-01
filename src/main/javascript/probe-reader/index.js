ca4366924a6eb591042aa9a1aa911fbc18ee7509
wgillaspy <w.gillaspy@gmail.com>
2020-07-01 18:28:51 -0400

    Reading the i2c device.

--- src/main/javascript/probe-reader/index.js
+++ src/main/javascript/probe-reader/index.js
@@ -3,7 +3,9 @@ const fs = require("fs-extra");
 
 const configuration = fs.readJsonSync('./configuration.json', 'utf8');
 
-setTimeout(library.readConfiguredProbesSync().then(result => {
-    console.log(JSON.stringify(result, null, 2));
-}), configuration.loop_time_in_milis);
+setTimeout(function() {
+    library.readConfiguredProbesSync().then(result => {
+        console.log(JSON.stringify(result, null, 2));
+    });
+}, configuration.loop_time_in_milis);
 
